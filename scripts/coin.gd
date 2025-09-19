944ec7dfcb8d06a9e11143ec59f5a3f3a82e1a70
gcater1 <geoffcater123@gmail.com>
2025-09-19 16:16:17 -0400

    is_inside_tree() error. used ChatGpt 5 code to add a safeguard in coin

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -4,5 +4,6 @@ extends Area2D
 
 
 func _on_body_entered(body:Node2D) -> void:
-	print("i'm a coin")
-	queue_free()
+    print("i'm a coin")
+    set_deferred("monitoring", false)   # stop more body_entered calls this frame
+    call_deferred("queue_free")         # free AFTER the signal finishes
