bdabe4b717a0538843f762a8cb476c96e0f95e31
gcater1 <geoffcater123@gmail.com>
2025-09-22 19:55:50 -0400

    player animation jump had issue. Deleted a space. Added a game manager that has function add score. Coin calls add score whenever it get collected

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
-
+@onready var game_manager = %GameManager
 
 
 func _on_body_entered(body:Node2D) -> void:
-    print("i'm a coin")
+    game_manager.add_point()
     set_deferred("monitoring", false)   # stop more body_entered calls this frame
     call_deferred("queue_free")         # free AFTER the signal finishes
