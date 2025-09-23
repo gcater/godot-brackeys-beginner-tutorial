f69e72438906cca9e76fce93a3c3a9f240948d7e
gcater1 <geoffcater123@gmail.com>
2025-09-23 14:30:41 -0400

    added music to the onready vars in project settings. Added an animation player to the coin pickup. everything related to pickup is now handeld by animation player

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
 @onready var game_manager = %GameManager
-
+@onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(body:Node2D) -> void:
-    game_manager.add_point()
-    set_deferred("monitoring", false)   # stop more body_entered calls this frame
-    call_deferred("queue_free")         # free AFTER the signal finishes
+	game_manager.add_point()
+	#set_deferred("monitoring", false)   # stop more body_entered calls this frame
+	#call_deferred(animation_player.play("pickup"))         # free AFTER the signal finishes
+	animation_player.play("pickup")
